81688cd79e0f808d9bba0fa65b6479d257e8b02a
las744-byte <las744@gmail.com>
2026-06-13 17:29:35 +0300

    test page

---
+++ app/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: 40, fontSize: 20 }}>
+      ✅ Сайт работает! <br />
+      Если ты это видишь — проблема в главной странице.
+    </div>
+  )
+}
