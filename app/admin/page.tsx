166b220041d8eb23cee7abebb14b9e9ec6bbee3d
las744-byte <las744@gmail.com>
2026-06-13 16:07:30 +0300

    fix admin page type

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,12 +1,10 @@
 'use client'
 
 import dynamic from 'next/dynamic'
-import { ComponentType } from 'react'
 
-const AdminApp = dynamic<ComponentType>(
-  () => import('@/components/AdminApp').then(mod => ({ default: mod.AdminApp })),
-  { ssr: false }
-)
+const AdminApp = dynamic(() => import('@/components/AdminApp').then(mod => ({ default: mod.AdminApp })), {
+  ssr: false,
+})
 
 export default function AdminPage() {
   return <AdminApp />
